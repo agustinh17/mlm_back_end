9bded9e4aa88340096b7426c623940cf515cacbb
agustinh17 <agustinh17@gmail.com>
2012-05-28 23:42:07 -0300

    create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MlmBackEnd::Application.config.secret_token = 'c9476101b08f5aba1c738b330d1aaa2316ebb1fd50ff556f496c4f6c2de218c18778cd7e687d321b458b1ec88845e4c68ecffd34ddc09682f5e2e60a3026f23c'
