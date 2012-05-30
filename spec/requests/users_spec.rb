b721762dc02f6da572d5e22f8ac1033b82210056
agustinh17 <agustinh17@gmail.com>
2012-05-30 14:08:36 -0300

    integration test for http basic authentication

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 
 describe "Users" do
-#  describe "GET /users" do
-#    it "works! (now write some real specs)" do
-#      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-#      get users_path
-#      response.status.should be(200)
-#    end
-#  end
+
   describe "GET /users" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+			credentials = ActionController::HttpAuthentication::Basic.encode_credentials('andy','thering')
+      get users_path, nil, {'HTTP_AUTHORIZATION' =>  credentials }
+
+      response.status.should be(200)
+    end
+
     it "requires authentication" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get users_path
